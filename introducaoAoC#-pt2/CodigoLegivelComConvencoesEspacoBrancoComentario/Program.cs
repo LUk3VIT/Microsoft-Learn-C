eadf4c700636d4cb2ad3286fdbd39ca187b5d194
Lucas Dias <91563939+LUk3VIT@users.noreply.github.com>
2026-03-17 23:28:25 -0300

    mais um passo

---
+++ introducaoAoC#-pt2/CodigoLegivelComConvencoesEspacoBrancoComentario/Program.cs
@@ -0,0 +1,41 @@
+﻿string firstName = "Bob";
+int widgetsPurchased = 7;
+// Testinga change to the message.
+// int widgetsSold = 7;
+// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} wigests");
+
+/*
+
+Essa forma de comentar se chama comentario em bloco sendo capaz de comentar varias linhas
+de uma vez, muito util
+
+*/
+
+//Desafio de código
+
+/*
+
+Objetivo do codigo é de saber quantos O tem na fraze, 
+depois que ela é invertidae e que frase ela cria desse jetito
+
+*/
+
+string message = "The quick brown fox jumps over the lazy dog.";
+int x = 0;
+
+char[] charMessage = message.ToCharArray();
+Array.Reverse(charMessage);
+
+foreach (char i in charMessage) 
+{ 
+    if (i == 'o') 
+    { 
+        x++; 
+    } 
+}
+
+string newMessage = new String(charMessage);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {x} times.");
---
+++ introducaoAoC#-pt2/ProjGuiado-EstruturarasForeachIfElseifElseProcessarDadosDeMatiz/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -0,0 +1,90 @@
+﻿int currentAssignments = 5;
+
+int[] sophiaNotas =  [90, 86, 87, 98, 100];
+int[] andrewNotas = [92, 89, 81, 96, 90];
+int[] emmaNotas = [90, 85, 87, 98, 68];
+int[] loganNotas = [90, 95, 87, 88, 96];
+
+string[] alunosNomes = ["Sophia", "Andrew", "Emma", "Logan"];
+int[] alunosNotas = [10];
+
+string alunoAtualGradeLetra = "";
+
+Console.WriteLine("Student\t\tGrade\n");
+
+foreach ( string nome in alunosNomes)
+{
+    string alunoAtual = nome;
+    
+    if(alunoAtual == "Sophia")
+        alunosNotas = sophiaNotas;
+
+    else if (alunoAtual == "Andrew")
+        alunosNotas = andrewNotas;
+    
+    else if (alunoAtual == "Emma")
+        alunosNotas = emmaNotas;
+
+    else if (alunoAtual == "Logan")
+        alunosNotas = loganNotas;
+
+
+    int sumNotasGrades = 0;
+
+    decimal alunoAtualGrade;
+
+    foreach (int nota in alunosNotas)
+    {
+        sumNotasGrades += nota;
+    }
+
+    alunoAtualGrade = (decimal)sumNotasGrades / currentAssignments;
+
+    if (alunoAtualGrade >= 97)
+        alunoAtualGradeLetra = "A+";
+
+    else if (alunoAtualGrade >= 93)
+        alunoAtualGradeLetra = "A";
+
+    else if (alunoAtualGrade >= 90)
+        alunoAtualGradeLetra = "A-";
+
+    else if (alunoAtualGrade >= 87)
+        alunoAtualGradeLetra = "B+";
+
+    else if (alunoAtualGrade >= 83)
+        alunoAtualGradeLetra = "B";
+
+    else if (alunoAtualGrade >= 80)
+        alunoAtualGradeLetra = "B-";
+
+    else if (alunoAtualGrade >= 77)
+        alunoAtualGradeLetra = "C+";
+
+    else if (alunoAtualGrade >= 73)
+        alunoAtualGradeLetra = "C";
+
+    else if (alunoAtualGrade >= 70)
+        alunoAtualGradeLetra = "C-";
+
+    else if (alunoAtualGrade >= 67)
+        alunoAtualGradeLetra = "D+";
+
+    else if (alunoAtualGrade >= 63)
+        alunoAtualGradeLetra = "D";
+
+    else if (alunoAtualGrade >= 60)
+        alunoAtualGradeLetra = "D-";
+
+    else
+        alunoAtualGradeLetra = "F";
+
+
+
+    Console.WriteLine($"{alunoAtual}\t\t{alunoAtualGrade}\t{alunoAtualGradeLetra}"); 
+    
+}
+
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
