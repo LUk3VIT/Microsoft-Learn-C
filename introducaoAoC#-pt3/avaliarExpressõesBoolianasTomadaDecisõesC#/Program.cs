8f66a138751474f09095bdb480206e7c7f3fe59d
LUCAS LEONARDO DOS SANTOS DIAS (2400572) <2400572@edu.anchieta.br>
2026-03-20 19:18:21 -0300

    Avançando

---
+++ introducaoAoC#-pt3/avaliarExpressõesBoolianasTomadaDecisõesC#/Program.cs
@@ -0,0 +1,10 @@
+﻿string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin")){
+    string validacaoLevel = level > 55 ? "Welcome, Super Admin user." : level <= 55 ? "Welcome, Admin user." : level >= 20 ? "Contact an Admin for access." : "You do not have sufficient privileges.";
+} else {
+    string validacaoPermission = "You do not have sufficient privileges.";
+}
+
+Console.WriteLine($"{validacaoLevel} {validacaoPermission}");
